e6a31c2e262bbba451dbc9c2b45a4f286100a1fc
Bonfeb <bonfebdevs@gmail.com>
2025-04-21 04:12:33 +0300

    adminTeam css

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import "./assets/css/Register.css";
 import "./assets/css/Service.css";
 import "./assets/css/Reviews.css";
 import "./assets/css/AdminReviews.css";
+import "./assets/css/AdminTeam.css";
 import Profile from "./pages/Profile";
 import Booking from "./pages/Booking";
 import ProfileUpdate from "./pages/ProfileUpdate";
